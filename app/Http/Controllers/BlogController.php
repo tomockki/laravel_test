bdd83aa012492a123bfa3a2482275ff0fd80aef0
tomockki <tomockki@yahoo.co.jp>
2021-04-10 20:45:09 +0900

    controller

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * ブログ一覧を表示
+     *
+     * @return view
+     */
+    public function showKist(){
+        return view('blog.list');
+
+    }
+}
